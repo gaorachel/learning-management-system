2b999c84d2c63b87b5a2d49a41dce63518eb53f7
Rachel <gggggrea@gmail.com>
2023-09-24 16:47:00 +0100

    Setup layout

---
+++ app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,7 @@
+import React, { ReactNode } from "react";
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return <div className="h-full flex items-center justify-center"> {children} </div>;
+};
+
+export default layout;
