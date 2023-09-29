48530a0875b5334e4ccefda76caeac25cd788711
Rachel <gggggrea@gmail.com>
2023-09-29 21:44:43 +0100

    Fix typo

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -6,6 +6,7 @@ import { db } from "@/lib/db";
 import { IconBadge } from "@/components/icon-badge";
 
 import { TitleForm } from "./_components/title-form";
+import { DescriptionForm } from "./_components/description-form";
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   const { userId } = auth();
@@ -30,17 +31,18 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
   return (
     <div className="p-6 ">
       <div className="flex items-center justify-between">
-        <div className="flex flex-col gay-y-2">
+        <div className="flex flex-col gap-y-2">
           <h1 className="text-2xl font-medium"> Course Setup </h1>
           <span className="text-sm text-slate-700"> Complete all fields {completionText} </span>
         </div>
       </div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-16 ">
-        <div className="flex items-center gay-x-2">
+        <div className="flex items-center gap-x-2">
           <IconBadge icon={LayoutDashboard} />
           <h2 className="text-xl"> Customise your course </h2>
         </div>
         <TitleForm initialData={course} courseId={course.id} />
+        <DescriptionForm initialData={course} courseId={course.id} />
       </div>
     </div>
   );
