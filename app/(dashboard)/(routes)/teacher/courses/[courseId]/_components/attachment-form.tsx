ccbb4151c3f823a1e9921da1e2496f96d7cbaead
Rachel <gggggrea@gmail.com>
2023-10-07 14:49:18 +0100

    Create attachment deletion feature

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/_components/attachment-form.tsx
@@ -2,7 +2,7 @@
 
 import * as z from "zod";
 import axios from "axios";
-import { ImageIcon, PlusCircle } from "lucide-react";
+import { File, Loader2, PlusCircle, X } from "lucide-react";
 import { useState } from "react";
 import { Attachment, Course } from "@prisma/client";
 import toast from "react-hot-toast";
@@ -22,6 +22,7 @@ const formSchema = z.object({
 
 export const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) => {
   const [isEditing, setIsEditing] = useState(false);
+  const [deletingId, setDeletingId] = useState<string | null>(null);
 
   const toggleEdit = () => setIsEditing((current) => !current);
 
@@ -35,6 +36,19 @@ export const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) =
       router.refresh();
     } catch {
       toast.error("Something went wrong");
+    }
+  };
+
+  const onDelete = async (id: string) => {
+    try {
+      setDeletingId(id);
+      await axios.delete(`/api/courses/${courseId}/attachments/${id}`);
+      toast.success("Attachment deleted");
+      router.refresh();
+    } catch (error) {
+      toast("Something went wrong");
+    } finally {
+      setDeletingId(null);
     }
   };
 
@@ -57,6 +71,34 @@ export const AttachmentForm = ({ initialData, courseId }: AttachmentFormProps) =
         <>
           {initialData.attachments.length === 0 && (
             <p className="text-sm mt-2 text-slate-500 italic"> No attachments yet </p>
+          )}
+
+          {initialData.attachments.length > 0 && (
+            <div className="space-y-2">
+              {initialData.attachments.map((attachment) => (
+                <div
+                  key={attachment.id}
+                  className="flex items-center p-3 w-full bg-sky-100 border-sky-200 border text-sky-700 rounded-md"
+                >
+                  <File className="h-4 w-4 mr-2 flex-shrink-0" />
+                  <p className="text-xs line-clamp-1"> {attachment.name} </p>
+                  {deletingId === attachment.id && (
+                    <div>
+                      <Loader2 className="h-4 w-4 animate-spin" />
+                    </div>
+                  )}
+                  {deletingId !== attachment.id && (
+                    <button
+                      type="button"
+                      className="ml-auto hover:opacity-75 transition"
+                      onClick={() => onDelete(attachment.id)}
+                    >
+                      <X className="h-4 w-4" />
+                    </button>
+                  )}
+                </div>
+              ))}
+            </div>
           )}
         </>
       )}
