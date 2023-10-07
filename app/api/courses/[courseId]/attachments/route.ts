23f00d7f7cbf4cc2e8136a1d86de0006893a2d44
Rachel <gggggrea@gmail.com>
2023-10-07 14:02:11 +0100

    Create course attachments post api

---
+++ app/api/courses/[courseId]/attachments/route.ts
@@ -0,0 +1,34 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+import { db } from "@/lib/db";
+
+export async function POST(req: Request, { params: { courseId } }: { params: { courseId: string } }) {
+  try {
+    const { userId } = auth();
+    const { url } = await req.json();
+
+    if (!userId) return new NextResponse("Unauthorised", { status: 401 });
+
+    const courseOwner = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+    });
+
+    if (!courseOwner) return new NextResponse("Unauthorised", { status: 401 });
+
+    const attachment = await db.attachment.create({
+      data: {
+        url,
+        name: url.split("/").pop(),
+        courseId,
+      },
+    });
+
+    return NextResponse.json(attachment);
+  } catch (error) {
+    console.log("COURSE_ID_ATTACHMENTS", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
