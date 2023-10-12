84a7248650f05ab1f83276a752b5cab63b70aa15
Rachel <gggggrea@gmail.com>
2023-10-12 23:38:26 +0100

    Add functionality on chapter action buttons

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -1,24 +1,48 @@
 "use client";
-
-import { Button } from "@/components/ui/button";
+import axios from "axios";
+import toast from "react-hot-toast";
 import { Trash } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+import { ConfirmModal } from "@/components/modals/confirm-modal";
+import { Button } from "@/components/ui/button";
 
 interface ChapterActionsProps {
-  disable: boolean;
+  disabled: boolean;
   courseId: string;
   chapterId: string;
   isPublished: boolean;
 }
 
-export const ChapterActions = ({ disable, courseId, chapterId, isPublished }: ChapterActionsProps) => {
+export const ChapterActions = ({ disabled, courseId, chapterId, isPublished }: ChapterActionsProps) => {
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const onDelete = async () => {
+    try {
+      setIsLoading(true);
+
+      await axios.delete(`/api/courses/${courseId}/chapters/${chapterId}`);
+
+      toast.success("Chapter deleted");
+      router.refresh();
+      router.push(`/teacher/courses/${courseId}`);
+    } catch (error) {
+      toast.error("Something went wrong");
+    }
+  };
   return (
     <div className="flex items-center gap-x-2">
-      <Button type="button" onClick={() => {}} disabled={disable} variant="outline" size="sm">
+      <Button type="button" onClick={() => {}} disabled={disabled || isLoading} variant="outline" size="sm">
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
-      <Button type="button" size="sm">
-        <Trash className="h-4 w-4" />
-      </Button>
+
+      <ConfirmModal onConfirm={onDelete}>
+        <Button type="button" size="sm" disabled={isLoading}>
+          <Trash className="h-4 w-4" />
+        </Button>
+      </ConfirmModal>
     </div>
   );
 };
