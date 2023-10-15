6803a325594da7022292170641aabac264be2bf9
Rachel <gggggrea@gmail.com>
2023-10-15 20:26:05 +0100

    Add publishing chapter feature

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/chapters/[chapterId]/_components/chapter-actions.tsx
@@ -19,6 +19,26 @@ export const ChapterActions = ({ disabled, courseId, chapterId, isPublished }: C
   const router = useRouter();
   const [isLoading, setIsLoading] = useState(false);
 
+  const onClick = async () => {
+    try {
+      setIsLoading(true);
+
+      if (isPublished) {
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/unpublish`);
+        toast.success("Chapter unpublished");
+      } else {
+        await axios.patch(`/api/courses/${courseId}/chapters/${chapterId}/publish`);
+        toast.success("Chapter published");
+      }
+
+      router.refresh();
+    } catch (error) {
+      toast.error("Something went wrong");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   const onDelete = async () => {
     try {
       setIsLoading(true);
@@ -34,7 +54,7 @@ export const ChapterActions = ({ disabled, courseId, chapterId, isPublished }: C
   };
   return (
     <div className="flex items-center gap-x-2">
-      <Button type="button" onClick={() => {}} disabled={disabled || isLoading} variant="outline" size="sm">
+      <Button type="button" onClick={onClick} disabled={disabled || isLoading} variant="outline" size="sm">
         {isPublished ? "Unpublish" : "Publish"}
       </Button>
 
