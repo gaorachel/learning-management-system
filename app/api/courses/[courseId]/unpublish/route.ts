65cef028af647ca17e930ccd0907e1a3d69dba48
Rachel <gggggrea@gmail.com>
2023-10-16 22:05:39 +0100

    Create course publishment API

---
+++ app/api/courses/[courseId]/unpublish/route.ts
@@ -0,0 +1,36 @@
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+import { db } from "@/lib/db";
+
+export async function PATCH(req: Request, { params: { courseId } }: { params: { courseId: string } }) {
+  try {
+    const { userId } = auth();
+
+    if (!userId) return new NextResponse("Unauthorised", { status: 401 });
+
+    const course = await db.course.findUnique({
+      where: {
+        id: courseId,
+        userId,
+      },
+    });
+
+    if (!course) return new NextResponse("Not found", { status: 404 });
+
+    const unpublishedCourse = await db.course.update({
+      where: {
+        id: courseId,
+        userId,
+      },
+      data: {
+        isPublished: false,
+      },
+    });
+
+    return NextResponse.json(unpublishedCourse);
+  } catch (error) {
+    console.log("[COURSE_ID_UNPUBLISH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
