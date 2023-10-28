db8c0c5355d7d1aa59ddcd1a1302e25171b8dd1b
Rachel <gggggrea@gmail.com>
2023-10-28 22:09:28 +0100

    Reorder imports

--- app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
+++ app/(course)/courses/[courseId]/chapters/[chapterId]/page.tsx
@@ -1,15 +1,15 @@
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { File } from "lucide-react";
 
 import { getChapter } from "@/actions/get-chapter";
 import { Banner } from "@/components/ui/banner";
 import { Separator } from "@/components/ui/separator";
+import { Preview } from "@/components/preview";
 
 import { VideoPlayer } from "./_components/video-player";
 import { CourseEnrollButton } from "./_components/course-enroll-button";
 import { CourseProgressButton } from "./_components/course-progress-button";
-import { Preview } from "@/components/preview";
-import { File } from "lucide-react";
 
 const ChapterIdPage = async ({
   params: { courseId, chapterId },
